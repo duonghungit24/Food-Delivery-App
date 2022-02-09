21040787f23a55e614f32eaa394ed587b0ac830d
duonghungit24 <hungduonng721@gmail.com>
2022-02-09 22:16:44 +0700

    done component HomeHeader

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {Colors, Parameters, Title} from '../global/styles';
-import {Icon} from 'react-native-elements';
+import {Icon , withBadge } from 'react-native-elements';
+
+const BadgeIcon = withBadge(0)(Icon);
 
 export default function HomeHeader() {
     return(
         <View style={styles.container}>
-            <View style={{justifyContent: 'center', marginLeft:10}}>
+            <View style={{marginLeft:10}}>
                 <Icon 
                     type="material-community"
                     name = "menu"
+                    size={35}
+                    color={Colors.cardbackground}
+                />
+            </View>
+            <View>
+                <Text style={styles.headerTitle}>App Food</Text>
+            </View>
+            <View style={{marginRight:10}}>
+                <BadgeIcon 
+                    type="material-community"
+                    name="cart"
                     size={35}
                     color={Colors.cardbackground}
                 />
@@ -22,5 +35,12 @@ const styles = StyleSheet.create({
         flexDirection:'row',
         backgroundColor:Colors.buttons,
         height:Parameters.headerHeight,
-    }
+        justifyContent: 'space-between', 
+        alignItems: 'center',
+    },
+     headerTitle: {
+         color: Colors.cardbackground,
+         fontSize:25,
+         fontWeight: 'bold',
+     }
 })
